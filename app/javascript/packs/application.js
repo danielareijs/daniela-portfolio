d97922e0e3d650b282a5261bd7691d5de4add145
daniela <danielareijs@gmail.com>
2021-01-17 23:37:43 +0100

    installed jquery ui, sortable html5 library and css calls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,8 @@ ActiveStorage.start()
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery-ujs
+//= require jquery-ui
+//= require html.sortable
+//= require turbolinks
+//= require_tree . 
